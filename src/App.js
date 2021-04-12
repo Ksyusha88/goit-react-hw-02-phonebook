f92aadfcd991db2d7ee7de0d1f00484ab5707d30
unknown <68596940+Ksyusha88@users.noreply.github.com>
2021-04-12 21:49:39 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { Component } from 'react';
+import Form from './Components/Form/';
+import shortid from 'shortid';
+import ContactList from './Components/ContactList';
+import ContactFilter from './Components/ContactFilter';
+import Container from './Components/Container';
+
+class App extends Component{
+  state = {
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  };
+
+formSubmitHandler = data => {
+  const newData = data;
+  return newData;
+};
+
+addContact = data => {
+  const newContact = data;
+  if (this.state.contacts.some(name => name.name === newContact.name))
+  return alert (`${newContact.name} is already in contacts.`)
+  newContact.id = shortid.generate();
+    return this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+deleteContact =(contactId) =>{
+  this.setState(prevState =>({
+    contacts: prevState.contacts.filter(contact => contact.id !== contactId )
+  }));
+};
+
+filrerContact = e => {
+  this.setState({ filter: e.currentTarget.value })
+}
+
+getVisibleContacts =() => {
+  const { filter, contacts } = this.state;
+  const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({name}) => 
+    name.toLowerCase().includes(normalizedFilter),
+    );
+}; 
+
+
+render (){
+
+const {filter} = this.state;
+const visibleContacts = this.getVisibleContacts();
+
+  return(
+    <Container>
+      <h1>Phonebook</h1> 
+      <Form onAddContact = {this.addContact}
+     />
+     <ContactFilter value ={filter} onChange = {this.filrerContact}/>
+      <ContactList contacts = {visibleContacts} onDeleteContact ={this.deleteContact}
+      />
+      </Container>
+    );
+  }
+};
+
+export default App;
